a3bc9f56515232122961e258a94a21e4226c1ae0
HenrikCheng <henrikcheng@live.se>
2023-06-14 12:55:46 +0200

    basic working example graphql

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-// const { GraphQLSchema, GraphQLObjectType } = require("graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
 const app = express();
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "HelloWorld",
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => "HelloWorld",
+      },
+    }),
+  }),
+});
 
 app.use(
   "/graphql",
   graphqlHTTP({
+    schema: schema,
     graphiql: true,
   })
 );
